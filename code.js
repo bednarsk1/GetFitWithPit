ec6cf2c9e68bfb38e7bf048597329482183a3109
Piotr Bednarski <163442748+bednarsk1@users.noreply.github.com>
2024-06-12 15:44:31 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,32 @@
+//
+//function responsible for responsive navbar (mobile version)
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
+
+
+//function responsible for slide-in animations of elements
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+//function..
